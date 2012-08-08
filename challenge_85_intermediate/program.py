c7bc030038cd4ca75b701a713222057ba6da143d
Chris Horton <chorton2227@gmail.com>
2012-08-08 12:05:02 -0400

    Remove extra .join() fixes #1

--- challenge_85_intermediate/program.py
+++ challenge_85_intermediate/program.py
@@ -4,7 +4,7 @@ def draw_ascii_3d_box(length, height, depth):
 	sys.stdout.write(''.join((' ' * (depth - i) + ':' * (length - 1) + '/' + '+' * i +'\n') for i in range(depth)))
 	
 	for i in range(height):
-		line = ''.join('#' for _ in range(length)).join()
+		line = ''.join('#' for _ in range(length))
 		if height - i > depth:
 			line += '+' * depth
 		else:
