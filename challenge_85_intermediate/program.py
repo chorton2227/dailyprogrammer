eb5daba98c33309997f3e268e6d985bbdc14f699
Chris Horton <chorton2227@gmail.com>
2012-08-08 10:46:17 -0400

    Challenge 85 Intermediate

---
+++ challenge_85_intermediate/program.py
@@ -0,0 +1,18 @@
+import sys
+
+def draw_ascii_3d_box(length, height, depth):
+	sys.stdout.write(''.join((' ' * (depth - i) + ':' * (length - 1) + '/' + '+' * i +'\n') for i in range(depth)))
+	
+	for i in range(height):
+		line = ''.join('#' for _ in range(length)).join()
+		if height - i > depth:
+			line += '+' * depth
+		else:
+			line += '+' * (height - (i + 1))
+		print line
+
+if len(sys.argv)==1:
+	print 'Enter arguments'
+	exit(0)
+	
+draw_ascii_3d_box(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]))
